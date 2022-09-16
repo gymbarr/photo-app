07c39bdf770c1ad6bf98e3e53a79b0281dfceb8b
Andrey Timakhovich <stewart96.at@gmail.com>
2022-09-16 21:17:49 +0300

    bug fix with credit card form javascript

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  
   var show_error, stripeResponseHandler, submitHandler;
   
   submitHandler = function (event) {
@@ -41,7 +40,7 @@ $(document).ready(function() {
   };
   show_error = function (message) {
     if($("#flash-messages").size() < 1){
-      $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
+      $('.container .main').prepend("<div id='flash-messages'></div>")
     }
     $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
     $('.alert').delay(5000).fadeOut(3000);
